b53bc2b9c78abcd8f012309ff6eb75a03d5e05bd
calvin d. woo <calvin.d.woo@gmail.com>
2024-06-09 19:12:13 -0400

    liveness analysis

--- compiler_register_allocator.py
+++ compiler_register_allocator.py
@@ -15,16 +15,52 @@ class Compiler(compiler.Compiler):
     ###########################################################################
 
     def read_vars(self, i: instr) -> Set[location]:
-        # YOUR CODE HERE
-        pass
+        match i:
+            case Instr(opcode, args):
+                if opcode in ["addq", "subq", "movq", "negq"]:
+                    rvars = set()
+                    for a in args:
+                        if isinstance(a, Variable) or isinstance(a, Reg):
+                            rvars.add(a)
+                    return rvars
+                else:
+                    return set()
+            case Callq(f, arity):
+                arg_regs = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]
+                return set([
+                    Reg(r) for r in arg_regs[:arity]
+                ])
+            case _:
+                return set()
 
     def write_vars(self, i: instr) -> Set[location]:
-        # YOUR CODE HERE
-        pass
+        match i:
+            case Instr(opcode, args):
+                if opcode in ["addq", "subq", "movq", "negq"]:
+                    return set([args[-1]])
+                else:
+                    return set()
+            case Callq(f, arity):
+                # return caller-saved registers
+                caller_saved_regs = ["rax", "rcx", "rdx", "rsi", "rdi", "r8", "r9", "r10", "r11"]
+                return set([
+                    Reg(r) for r in caller_saved_regs
+                ])
+            case _:
+                return set()
 
     def uncover_live(self, p: X86Program) -> Dict[instr, Set[location]]:
-        # YOUR CODE HERE
-        pass
+        lives = []
+        for i in reversed(p.body):
+            if len(lives) == 0:
+                lives.append((i, set()))
+            else:
+                _, prev_live = lives[-1]
+                rvars = self.read_vars(i)
+                wvars = self.write_vars(i)
+                live = prev_live.difference(wvars).union(rvars)
+                lives.append((i, live))
+        return {i: lv for (i, lv) in lives}
 
     ############################################################################
     # Build Interference
